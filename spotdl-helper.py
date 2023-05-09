d8fa51b954eae53cb97ea58cb68e2c792c0d07ee
ArcWandx86 <arcwandx86@gmail.com>
2023-05-09 13:07:21 -0400

    working on verification and renaming

--- spotdl-helper.py
+++ spotdl-helper.py
@@ -1,4 +1,5 @@
-#  import pandas as pd
+from yt_dlp import YoutubeDL
+import simplejson as json
 import subprocess
 import sys
 import os
@@ -195,14 +196,161 @@ def replace_songs(replace_list):
 
     # Delete the replaced songs from the main buffer
     for file in os.listdir(RULES['BUFFER']):
-        if len(file.split('.')) < 2:
+        if len(file.split('.')) < 3:
             continue
         if file.split('.')[-2] in spotids:
             os.remove(os.path.join(RULES['BUFFER'], file))
 
-### Manual replace songs ###
-
 ### \Manual replace songs ###
+
+
+### Remove IDs ###
+
+# Remove IDs from the files in the buffer
+def remove_ids(buffer):
+    for file in os.listdir(buffer):
+        if len(file.split('.')) < 3:
+            continue
+        os.rename(os.path.join(buffer, file),
+                  os.path.join(buffer, '.'.join(file.split('.')[:-1]) + '.' + file.split('.')[-1]))
+
+### \Remove IDs ###
+
+
+### Duplicate check ###
+
+# Check for duplicates
+def duplicate_check(level):
+    if level == 0 or RULES['MODE'] != 'merge':
+        return
+
+    # Get the list of songs in the buffer
+    buffer_songs = os.listdir(RULES['BUFFER'])
+    # Get the list of songs in the output directory
+    output_songs = set(os.listdir(RULES['OUTPUT']))
+
+    # Get the list of songs that are in both the buffer and the output
+    duplicates = [s for s in buffer_songs if s in output_songs]
+
+    match level:
+        case 1: # Delete the duplicates from the buffer
+            for file in duplicates:
+                os.remove(os.path.join(RULES['BUFFER'], file))
+        case 2: # Delete the duplicates from the existing directory
+            for file in duplicates:
+                os.remove(os.path.join(RULES['OUTPUT'], file))
+        case 3: # Manual review
+            if len(duplicates) == 0:
+                return
+
+            with open('duplicates.txt', 'w') as f:
+                f.write('\n'.join(duplicates))
+            print(f'{len(duplicates)} duplicates found.\n')
+            if len(duplicates) < 15:
+                print('Duplicates:')
+                print('\n'.join(duplicates))
+
+            print('Duplicates written to duplicates.txt')
+            print('Please remove the duplicates and run again.')
+            exit(4)
+
+### \Duplicate check ###
+
+
+### Verification ###
+
+# Get the metadata of the songs in the buffer using ffprobe
+def get_ffprobe_data():
+    metadata = {}
+
+    # Download the metadata of the songs in the buffer
+    ffprobe_cmd = ['ffprobe', '-v', '0', '-show_entries', 'format']
+    
+    for file in os.listdir(RULES['BUFFER']):
+        title = ''
+        artist = ''
+        album = ''
+        url = ''
+
+        result = subprocess.run(ffprobe_cmd + [os.path.join(RULES['BUFFER'], file)], capture_output=True, text=True)
+        if result.returncode != 0:
+            print(result.stderr)
+            exit(5)
+
+        for line in result.stdout.split('\n'):
+            if line.startswith('TAG:title='):
+                title = line.split('TAG:title=')[1]
+            elif line.startswith('TAG:artist='):
+                artist = line.split('TAG:artist=')[1]
+            elif line.startswith('TAG:album='):
+                album = line.split('TAG:album=')[1]
+            elif line.startswith('TAG:comment=https://'):
+                url = line.split('TAG:comment=')[1]
+
+        metadata[file] = (title, artist, album, url)
+
+    return metadata
+
+# Get the metadata of the songs in the buffer using yt-dlp
+def get_yt_data(fileurls):
+    metadata = {}
+
+    with YoutubeDL() as ydl:
+        for filename, url in fileurls.items():
+            data = json.loads(json.dumps(ydl.sanitize_info(
+                ydl.extract_info(url, download=False))))
+
+            title, creator, channel, album = '', '', '', ''
+            try:
+                title = data['title']
+            except KeyError:
+                pass
+            try:
+                creator = data['creator']
+            except KeyError:
+                pass
+            try:
+                channel = data['channel']
+            except KeyError:
+                pass
+            try:
+                album = data['album']
+            except KeyError:
+                pass
+
+            metadata[filename] = (title, creator, channel, album)
+
+    return metadata
+
+# Verify that the correct songs were downloaded
+def verify(level):
+    if level == 0:
+        return
+
+    # { filename: (title, artist, album, url)}
+    metadata = get_ffprobe_data()
+    # { filename: (title, creator, channel, album)}
+    yt_metadata = get_yt_data({ file: data[3] for file, data in metadata.items() })
+
+    # Make sure that title and artist match
+    verification_queue = []
+    for file, (title, artist, album, _), _, (yt_title, creator, channel, yt_album) in zip(
+        metadata.keys(), metadata.values(), yt_metadata.keys(), yt_metadata.values()):
+        if title != yt_title:
+            verification_queue.append(file)
+        if artist != creator and artist != channel:
+            verification_queue.append(file)
+        if level == 2 or level == 4 and album != yt_album:
+            verification_queue.append(file)
+
+    print('Metadata: ')
+    print(metadata)
+    print('Youtube metadata: ')
+    print(yt_metadata)
+    print('Verification queue: ')
+    print(verification_queue)
+
+### \Verification ###
 
 
 ### Main ###
@@ -215,8 +363,11 @@ def main():
     parser(filename, RULES)
 
     funcs = [
-        (download_songs, (RULES['URL'], RULES['BUFFER'])),
-        (replace_songs, (RULES['REPLACE'],)),
+        (download_songs, [ RULES['URL'], RULES['BUFFER'] ]),
+        (replace_songs, [ RULES['REPLACE'] ]),
+        (remove_ids, [ RULES['BUFFER'] ]),
+        (duplicate_check, [ RULES['DUP-SCAN-LEVEL'] ]),
+        (verify, [ RULES['VERIFY-LEVEL'] ]),
     ]
 
     list(map(lambda z: z[0](*z[1]), funcs[RULES['SKIP']:]))
